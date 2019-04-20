e0daaba3a3a808813364e739cad1b1d404786ee6
dodoan <dodoan.it@gmail.com>
2019-04-20 21:50:23 +0700

    - added intro scene

---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour {
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
