32ce6d5e89ea98a71e2e47e53f988d87b3033e40
dodoan <dodoan.it@gmail.com>
2019-04-20 17:37:06 +0700

    - cleanup the code

--- Assets/Scripts/Logic/PlayerManager.cs
+++ Assets/Scripts/Logic/PlayerManager.cs
@@ -10,28 +10,11 @@ public class PlayerManager : MonoBehaviour
 
     [SerializeField]
     private List<GameObject> cardsInHand = new List<GameObject>();
-    private List<float> cardsPositionX = new List<float> { 0, -3f, 3f, -6f, 6f };
 
     // Start is called before the first frame update
     void Start()
     {
-        /*for(int i = 0; i < cardsAsset.Count; i++)    
-        {
-            GameObject card;
-            if(isPlayer)
-            {
-                card = GameObject.Instantiate(CreatureCardPrefab, new Vector3(cardsPositionX[i], -3f, 0f), Quaternion.Euler(new Vector3(0f, 0f, 0f))) as GameObject;
-            }
-            else
-            {
-                card = GameObject.Instantiate(CreatureCardPrefab, new Vector3(cardsPositionX[i], 5f, 0f), Quaternion.Euler(new Vector3(0f, -179f, 0f))) as GameObject;
-                card.GetComponent<HoverPreview>().ThisPreviewEnabled = false;
-            }
 
-            card.GetComponent<CardManager>().UpdateCardInformantion(cardsAsset[i]);
-
-            AddCard(card);
-        }*/
     }
 
     // Update is called once per frame
