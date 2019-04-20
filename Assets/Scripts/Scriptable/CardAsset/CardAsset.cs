617a2d8d773f3b87177e87f5b9dc5390f0d80251
dodoan <dodoan.it@gmail.com>
2019-04-20 12:48:46 +0700

    - card scriptable added

---
+++ Assets/Scripts/Scriptable/CardAsset/CardAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardAsset : ScriptableObject 
+{	
+    [Header("Card Info")]
+    [TextArea(2,3)]
+    public string Description;
+	public Sprite CardImage;
+    public int Health;
+    public int Attack;
+}
